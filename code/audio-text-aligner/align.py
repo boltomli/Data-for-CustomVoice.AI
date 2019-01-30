a2b894a26e47e6ed8af8dcd267947dc4f5aeff54
Song Li <songl@outlook.com>
2019-01-30 18:39:30 +0800

    Return zip file of split audio and text

--- code/audio-text-aligner/align.py
+++ code/audio-text-aligner/align.py
@@ -5,7 +5,12 @@
 '''This module aligns audio and text'''
 
 import codecs
+import json
+import re
+import tempfile
 from os import linesep
+from os.path import join
+from zipfile import ZIP_DEFLATED, ZipFile
 
 import chardet
 import jieba
@@ -13,6 +18,7 @@ from aeneas.executetask import ExecuteTask
 from aeneas.task import Task
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize.punkt import PunktLanguageVars, PunktSentenceTokenizer
+from pydub import AudioSegment
 
 import utils
 
@@ -46,7 +52,36 @@ def process_files(text_file, audio_file, lang):
     task = Task(config_string=config_string)
     task.audio_file_path_absolute = audio_file
     task.text_file_path_absolute = text_file
-    task.sync_map_file_path_absolute = audio_file + '.json'
     ExecuteTask(task).execute()
-    task.output_sync_map_file()
-    return audio_file + '.json'
+    result = json.loads(task.sync_map.json_string)
+    with open(audio_file+'.txt', 'wb') as f:
+        f.write(codecs.BOM_UTF16_LE)
+    with tempfile.TemporaryDirectory() as temp_dir:
+        for fragment in result['fragments']:
+            file_id = re.sub(r'\D', '', fragment['id'])
+            if (file_id):
+                segment_audio(audio_file, float(fragment['begin']), float(fragment['end']), join(temp_dir, file_id))
+                append_zip(join(temp_dir, file_id), file_id, audio_file+'.zip')
+                append_txt(fragment['lines'], file_id, audio_file+'.txt')
+    with ZipFile(audio_file+'.audiozip_script.zip', 'w', compression=ZIP_DEFLATED) as f:
+        f.write(audio_file+'.zip', arcname='audio.zip')
+        f.write(audio_file+'.txt', arcname='script.txt')
+    return audio_file+'.audiozip_script.zip'
+
+
+def segment_audio(in_audio_file, time_begin, time_end, out_audio_file):
+    time_begin = time_begin * 1000
+    time_end = time_end * 1000
+    newAudio = AudioSegment.from_file(in_audio_file)
+    newAudio = newAudio[time_begin:time_end]
+    newAudio.export(out_audio_file, format="wav")
+
+
+def append_zip(in_audio_file, file_id, out_zip_file):
+    with ZipFile(out_zip_file, 'a', compression=ZIP_DEFLATED) as f:
+        f.write(in_audio_file, arcname=file_id+'.wav')
+
+
+def append_txt(text, file_id, out_txt_file):
+    with open(out_txt_file, 'ab') as f:
+        f.write((file_id+'\t'+' '.join(text)+'\n').encode('utf-16-le'))
